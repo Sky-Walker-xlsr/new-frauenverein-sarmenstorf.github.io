aecd6ddec76c060e520c788379ea62eb816cdf17
sky_walker_xlsr <yannicksalm07@gmail.com>
2025-09-04 09:50:53 +0200

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/new-frauenverein-sarmenstorf/',  // 👈 wichtig für GitHub Pages
+});
